444458a6dc6dddad5d14cb80e3b321669e8bc7c3
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-09-16 09:17:31 -0400

    Added more strings classifiers items for this package

--- setup.py
+++ setup.py
@@ -3,24 +3,41 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0'
+version = '2.0.1'
 
 setup(name='zettwerk.ui',
       version=version,
-      description="Adding jquery.ui's themeroller to plone 4 " \
+      description="Adding jQuery UI's ThemeRoller to Plone 4 " \
           "for easy theme customization.",
       long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      # https://pypi.org/pypi?:action=list_classifiers
       classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
         "Framework :: Plone",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: Theme",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='plone, theme, jquery, jquery.ui, themeroller',
+      keywords='plone theme jquery jquery.ui themeroller web zope cms',
       author='Jörg Kubaile / zettwerk GmbH',
       author_email='jk@zettwerk.com',
-      url='http://www.zettwerk.com',
+      url='https://github.com/collective/zettwerk.ui',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['zettwerk'],
